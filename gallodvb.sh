3315a1eadd33bf72afa34f785459394054c18af1
gallochri <chri@gallochri.com>
2015-02-15 16:37:57 +0100

    Update gallodvb.sh

--- gallodvb.sh
+++ gallodvb.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+echo "Cloning openbricks..."
 git clone https://github.com/OpenBricks/openbricks.git
+
+echo "Loading configs..."
 cp gallodvb.conf openbricks/config/defconfigs/
 cd openbricks
 ./scripts/loadcfg gallodvb
+
+make menuconfig
+make -j12
